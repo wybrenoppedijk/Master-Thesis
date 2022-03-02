c1f394db071c5af0a6bb6ad59ffe35c472ceeb67
Niels Tiben <nielstiben@outlook.com>
2022-03-02 15:04:58 +0100

    implement rest call frequency

--- read_voltage.py
+++ read_voltage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import sys
+
 import serial
 import requests
-from time import sleep
+from time import time_ns
+
 
 def send(a0, a1, a2, a3):
-    url = 'https://actionprojectdatacollector.azurewebsites.net/dtu/wybren/raspberry/measurements/4b0c2338-9a29-11ec-b909-0242ac120002'
+    url = 'https://actionprojectdatacollector.azurewebsites.net/dtu/wybren/raspberry/measurements'
     headers = {'Content-Type': 'application/json'}
     data = [
         {
@@ -23,6 +25,7 @@ def send(a0, a1, a2, a3):
     r = requests.post(url, headers=headers, json=data)
     print(r.status_code)
 
+
 def read():
     port = '/dev/ttyUSB0'
     if len(sys.argv) > 1 and sys.argv[1] == 'debug':
@@ -31,15 +34,15 @@ def read():
     ser = serial.Serial(port, 9600, timeout=1)
     ser.reset_input_buffer()
 
+    freq_in_sec = 2
+    waiting_time = freq_in_sec * 1000 * 1000 * 1000  # seconds to nanoseconds.
+    last_sent = time_ns()
     while True:
-        a0, a1, a2, a3 = None, None, None, None
-        ser.reset_input_buffer()
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
-            a0, a1, a2, a3 = line.split(',')
-            send(a0, a1, a2, a3)
-        sleep(2)
-
+            if time_ns() > last_sent + waiting_time:
+                send(*line.split(','))
+                last_sent = time_ns()
 
 
 if __name__ == '__main__':
